97c82b31accd45a9129f64ef507555192ac2f600
iliatcymbal <iliatcymbal@gmail.com>
2018-01-21 01:06:43 +0200

    Set up enzyme

--- src/app.component.test.js
+++ src/app.component.test.js
@@ -1,3 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 describe('first test', () => {
   it('sum 1 + 1 is 2', () => {
     expect(1 + 1).toBe(2);
