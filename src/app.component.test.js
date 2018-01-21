e95de56d15068e90f885eac73ff4c31de1725832
iliatcymbal <iliatcymbal@gmail.com>
2018-01-22 01:52:41 +0200

    Created fake fetch

--- src/app.component.test.js
+++ src/app.component.test.js
@@ -5,28 +5,28 @@ import { User } from './User';
 
 describe('AppComponent', () => {
   it('should create AppComponent', () => {
-    const wrapper = shallow(<AppComponent />);
+    const wrapper = shallow(<AppComponent/>);
     expect(wrapper.find('h1').length).toBe(1);
   });
 
   it('should have null user in state', () => {
-    const wrapper = shallow(<AppComponent />);
+    const wrapper = shallow(<AppComponent/>);
     expect(wrapper.state().user).toBeNull();
   });
 
   it('should show Login component on null user', () => {
-    const wrapper = shallow(<AppComponent />);
+    const wrapper = shallow(<AppComponent/>);
     expect(wrapper.find(Login).length).toBe(1);
   });
 
   it('should show User component on non-null user', () => {
-    const wrapper = shallow(<AppComponent />);
-    wrapper.setState({user: {} });
+    const wrapper = shallow(<AppComponent/>);
+    wrapper.setState({ user: {} });
     expect(wrapper.find(User).length).toBe(1);
   });
 
   it('should set new state on setUser()', () => {
-    const wrapper = shallow(<AppComponent />);
+    const wrapper = shallow(<AppComponent/>);
     const instance = wrapper.instance();
     const testData = {};
     instance.setUser(testData);
@@ -35,4 +35,22 @@ describe('AppComponent', () => {
     // expect(wrapper.state().user).toBe(testData);
   });
 
-});
+  it('should set users in state on fetch() users', async () => {
+    const user = { name: 'John' };
+    const users = [user, user];
+    const wrapper = shallow(<AppComponent/>);
+    const fakeFetch = () => ({
+      then: () => ({
+        then: (cb) => cb(users)
+      })
+    });
+
+    global.fetch = fakeFetch;
+
+    wrapper.find('button').simulate('click');
+
+    expect(wrapper.state().users).toBe(users);
+  });
+
+})
+;
